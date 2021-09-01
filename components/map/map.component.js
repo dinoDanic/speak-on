f2b298dac1a7908122de4a9ded96d3f8e795c94a
Dino Danic <ninadino@Dinos-MacBook-Pro.local>
2021-09-01 23:45:07 +0200

    fix

--- components/map/map.component.js
+++ components/map/map.component.js
@@ -16,10 +16,10 @@ const Map = () => {
       <iframe
         width="100%"
         height="400"
-        frameborder="0"
+        frameBorder="0"
         scrolling="no"
-        marginheight="0"
-        marginwidth="0"
+        marginHeight="0"
+        marginWidth="0"
         id="gmap_canvas"
         src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=%20Zagreb+()&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
       ></iframe>
